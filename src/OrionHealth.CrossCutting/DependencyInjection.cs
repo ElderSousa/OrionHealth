873797f83e65596928b7de1d52e4f15e9d0dccb0
ElderSousa <elder2faces@gmail.com>
2025-08-14 14:19:53 -0300

    Instalaçao de pacotes logger e adicionado ao DependencyInjection

--- src/OrionHealth.CrossCutting/DependencyInjection.cs
+++ src/OrionHealth.CrossCutting/DependencyInjection.cs
@@ -19,6 +19,8 @@ public static class DependencyInjection
 
         services.AddInfrastructure(configuration);
 
+        services.AddLogging();
+
         return services;
     }
 
--- src/OrionHealth.CrossCutting/obj/Debug/net8.0/OrionHealth.CrossCutting.AssemblyInfo.cs
+++ src/OrionHealth.CrossCutting/obj/Debug/net8.0/OrionHealth.CrossCutting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrionHealth.CrossCutting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d38b473409c27578abfd661d0e4d6706b521625")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c320c076604668a5cf194d8b3db9bb92ec0aa2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrionHealth.CrossCutting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrionHealth.CrossCutting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
